1dab74a4cb29c2e63cb29316f18d4e4aa282e9fa
dnakaznenk <nakaznenko.da@gmail.com>
2023-11-03 18:04:41 +0200

    a

--- pathfinder/src/mx_create_pathfinder.c
+++ pathfinder/src/mx_create_pathfinder.c
@@ -3,7 +3,8 @@
 t_pathfinder *mx_create_pathfinder(void) {
     t_pathfinder *pathfinder = (t_pathfinder *) malloc(sizeof(t_pathfinder));
     pathfinder->islands_count = 0;
-    pathfinder->islands_count = 0;
+    pathfinder->real_islands_count = 0;
+    pathfinder->paths_count = 0;
 
     pathfinder->matrix = NULL;
     pathfinder->islands = (char **)malloc(sizeof(char *));
